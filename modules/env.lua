6a0dafbebe6d4d2ba3b63d3610f3016ecc82af2c
Chizuko <ChizkaKomr>
2026-06-17 16:30:13 +0300

    бу

--- modules/env.lua
+++
@@ -1,8 +0,0 @@
-hl.env("XCURSOR_SIZE", "12")
-hl.env("HYPRCURSOR_SIZE", "12")
-
-hl.env("HYPRCURSOR_THEME", "Bibata-Modern-Classic")
-
-
-hl.env("GBM_BACKEND", "nvidia-drm")
-hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
