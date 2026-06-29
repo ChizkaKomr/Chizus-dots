04890084ccb9ad120dfc24f22d95a45e2312daba
Chizuko <ChizkaKomr>
2026-06-29 23:32:06 +0300

    штооо!!! чизукс сваи дотсы абнавила? ну маладээц, бекапчики эт харашо

--- hypr/modules/auto-start.lua
+++ hypr/modules/auto-start.lua
@@ -3,6 +3,7 @@ hl.on("hyprland.start", function ()
   hl.exec_cmd("kitty")
   hl.exec_cmd("systemctl --user --no-block start graphical-session.target")
   hl.exec_cmd("hypridle")
+  hl.exec_cmd("systemctl --user start hyprpolkitagent")
   hl.exec_cmd("waybar & awww-daemon")
 end)
 
